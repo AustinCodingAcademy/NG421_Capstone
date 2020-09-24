bb599d1161ac53a92a10213a203f6dd592c89470
mercesolis <mersolis@live.com>
2020-09-24 14:19:44 -0500

    add dbz data

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -28,5 +28,23 @@ namespace capstone.Data
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<DbzMember>()
+            .HasData
+            (new DbzMember { 
+                Id = 1, 
+                Name = "Goku", 
+                Attack = "Spirit Bomb, Kamehameha",
+                UserId = "00973448-2f4b-44ec-a2d9-dbbc9663eb93"},
+            new DbzMember { 
+                Id = 2, 
+                Name = "Vegeta", 
+                Attack = "Galick Gun",
+                UserId = "00973448-2f4b-44ec-a2d9-dbbc9663eb93"});
+
+            base.OnModelCreating(builder);
+        }
     }
 }
