1a9567346989d097208ece406c514bea557e3ce8
mercesolis <mersolis@live.com>
2020-09-25 11:22:42 -0500

    dbz controller and component

---
+++ Controllers/DbzMemberController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using capstone.Data;
+using capstone.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Security.Claims;
+
+namespace capstone.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DbzMemberController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+
+        public DbzMemberController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public <IEnumerable<DbzMember> Get()
+        {
+            var userId = HttpContext.User.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            return _context.DbzMembers.Where(m => m.UserId == userId);
+        }
+    }
+}
