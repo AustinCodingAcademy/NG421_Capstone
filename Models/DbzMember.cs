5c2f6957e4dafe8367f6e24f58415243d350c310
mercesolis <mersolis@live.com>
2020-09-28 14:03:35 -0500

    added data

--- Models/DbzMember.cs
+++ Models/DbzMember.cs
@@ -14,8 +14,8 @@ namespace capstone.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Attack { get; set; }
-        public string Defense {get;set;}
-        public string Notes {get;set;}
+        public string Defense { get; set;}
+        public string Notes { get; set;}
         public string UserId { get; set;}
         public ApplicationUser User { get; set;}
 
